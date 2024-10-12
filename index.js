049401f82f70a7980f3951d0d46c62e26898d4e6
JessiP23 <jessi316866@gmail.com>
2024-10-11 22:43:46 -0400

    query

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
-// collection
+// collection, query
 import { collection, query, where, getDocs } from 'firebase/firestore';
 
 //firebase
